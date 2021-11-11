a74d85c3a4bf56665e28726970dc17681441289c
Julio Quinchiguango <jcquinchiguango@utn.edu.ec>
2021-11-11 14:11:19 -0500

    Configuración de cadena de conexión

--- database/connection.js
+++ database/connection.js
@@ -3,11 +3,20 @@ const db_sequelize = new Sequelize(process.env.DATABASE, process.env.USER, proce
     host: process.env.HOST,
     dialect: 'postgres',
     /* logging: false, */
+    protocol: "postgres",
+    logging: true,
     ssl: true,
     omitNull: true,
+
     define: {
         "freezeTableName": true, // sirve para mantenerlos estaticos a los nombres de la tabla
         "timestamps": false // sirve para quitar los atributos por defecto como: createdAt:updatedAt:
+    },
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false // <<<<<< YOU NEED THIS
+        }
     }
 });
 
