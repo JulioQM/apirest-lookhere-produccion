6772316850848a62405fe47a46cda13a6bcddc89
Julio Quinchiguango <jcquinchiguango@utn.edu.ec>
2022-03-06 03:15:45 -0500

    Add function of autentication by Admin and User. Else add routes of Admin and User, in the class Autentication

--- controllers/autenticacion.controller.js
+++ controllers/autenticacion.controller.js
@@ -1,13 +1,17 @@
 const { response, request } = require('express');
 const bcryptjs = require('bcryptjs');
 const { Usuario } = require('../models/usuarios.model');
+const { Sequelize } = require('sequelize');
 //:::::::::::::: AUTENTICACIÓN ::::::::::::::::: LISTO
-
-const login = async(req, res = response) => {
+// Login de super usuario
+const loginAdmin = async(req, res = response) => {
 
     try {
         const { usua_alias, usua_clave } = req.body;
-        const usuario = await Usuario.findOne({ where: { usua_alias: usua_alias, usua_estado: '1' } });
+        // busco y realizo validaciones con operadores logicos
+        const usuario = await Usuario.findOne({
+            where: Sequelize.or({ usua_alias: usua_alias, usua_estado: '1', rol_id: 1 }, { usua_email: usua_alias, usua_estado: '1', rol_id: 1 })
+        });
 
         if (!usuario) {
             return res.status(400).json({
@@ -35,4 +39,41 @@ const login = async(req, res = response) => {
     }
 }
 
-module.exports = { login };
+// Login de usuarios normales
+const loginUser = async(req, res = response) => {
+
+    try {
+        const { usua_alias, usua_clave } = req.body;
+        /* const usuario = await Usuario.findOne({ where: { usua_alias: usua_alias, usua_estado: '1' } }); */
+        const usuario = await Usuario.findOne({
+            where: Sequelize.or({ usua_alias: usua_alias, usua_estado: '1', rol_id: 2 }, { usua_email: usua_alias, usua_estado: '1', rol_id: 2 })
+        });
+
+        if (!usuario) {
+            return res.status(400).json({
+                message: 'El usuario o la contraseña es incorrecta.'
+            });
+        }
+        // Des-encriptar la contraseña y verificar contraseña
+        const validarClave = bcryptjs.compareSync(usua_clave, usuario.usua_clave);
+        console.log(validarClave + '.........' + usua_clave + '-----------' + usua_clave)
+        if (!validarClave) {
+            return res.status(400).json({
+                message: 'El usuario o la contraseña es incorrecta.'
+            });
+        }
+        // Resultado final de autenticación
+        // message: 'Crendenciales correctas'
+        const { usua_id } = usuario;
+        return res.status(200).json({ usua_id });
+
+    } catch (error) {
+
+        return res.status(500).json({
+            message: `Error detectado: ${error}`
+        });
+    }
+}
+
+
+module.exports = { loginUser, loginAdmin };
--- routes/autenticacion.route.js
+++ routes/autenticacion.route.js
@@ -1,14 +1,22 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
-const { login } = require('../controllers/autenticacion.controller');
+const { loginAdmin, loginUser } = require('../controllers/autenticacion.controller');
 const { validarCampo } = require('../middlewares/validar-campos');
 
 const router = Router();
 // RUTA LOGIN DE USUARIO
-router.post("/login", [
+// Para la parte de administración
+router.post("/loginAdmin", [
     check('usua_alias', 'Ingrese su usuario!').notEmpty(),
     check('usua_clave', 'Ingrese su contraseña!').notEmpty(),
     validarCampo
-], login);
+], loginAdmin);
+// Para la parte de usuarios normales
+router.post("/loginUser", [
+    check('usua_alias', 'Ingrese su usuario!').notEmpty(),
+    check('usua_clave', 'Ingrese su contraseña!').notEmpty(),
+    validarCampo
+], loginUser);
+
 
 module.exports = router;
